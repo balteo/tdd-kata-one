41a91c88365e4caecd2b631d75c237572681026e
Julien Martin <balteo@gmail.com>
2018-09-12 15:40:39 +0200

    Initial commit

---
+++ src/main/java/tdd/kata/one/TddKataApplication.java
@@ -0,0 +1,12 @@
+package tdd.kata.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddKataApplication.class, args);
+	}
+}
